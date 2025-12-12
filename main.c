6eb12022c20f83a5417fd3b7b28616d9cd183b3e
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-12-12 09:50:15 +0100

    Testez cette fonction en lisant, dans un fichier, les caractères dont la position est multiple de 1. Testez à nouveau avec une position multiple de 8.

--- main.c
+++ main.c
@@ -20,6 +20,24 @@ int main() {
     for (int c; ((c = sgf_getc(file)) > 0); ) {
         putchar(c);
     }
+
+    //TEST
+    printf("\nTEST \n");
+        if (file == NULL) {
+            printf("Erreur ouverture fichier\n");
+            return EXIT_FAILURE;
+        }
+        for (int i = 0; i < file->inode.size; i += 2) {
+            if (sgf_seek(file, i) == -1) {
+                printf("Erreur seek a la position %d\n", i);
+                break;
+            }
+            int c = sgf_getc(file);
+            if (c != -1) {
+                putchar(c);
+            }
+        }
+        printf("\n");
     sgf_close(file);
     
     return (EXIT_SUCCESS);
