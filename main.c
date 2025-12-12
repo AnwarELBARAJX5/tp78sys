30cd17a88c331d856f7ce3cbff58e6e77d6afc26
AnwarELBARAJX5 <elbarrajanwar@gmail.com>
2025-12-12 10:09:05 +0100

    tester son bon fonctionnement avec le code ci-dessous

--- main.c
+++ main.c
@@ -16,29 +16,18 @@ int main() {
     list_directory();
     
     printf("\nCONTENU DE essai.txt\n\n");
-    OFILE *file = sgf_open("essai.txt", READ_MODE);
+    OFILE *file = sgf_open("essai.txt", WRITE_MODE);
     for (int c; ((c = sgf_getc(file)) > 0); ) {
         putchar(c);
     }
-
-    //TEST
-    printf("\nTEST \n");
-        if (file == NULL) {
-            printf("Erreur ouverture fichier\n");
-            return EXIT_FAILURE;
-        }
-        for (int i = 0; i < file->inode.size; i += 2) {
-            if (sgf_seek(file, i) == -1) {
-                printf("Erreur seek a la position %d\n", i);
-                break;
-            }
-            int c = sgf_getc(file);
-            if (c != -1) {
-                putchar(c);
-            }
-        }
-        printf("\n");
+     printf("TEST ECRITURE \n");
+         sgf_puts(file, "Ceci est un petit texte qui occupe\n");
+         sgf_puts(file, "quelques blocs sur ce disque fictif.\n");
+         sgf_puts(file, "Le bloc faisant 128 octets, il faut\n");
+         sgf_puts(file, "que je remplisse pour utiliser\n");
+         sgf_puts(file, "plusieurs blocs.\n");
     sgf_close(file);
-    
+    printf("Ecriture terminée. Verification de la lecture :\n");
+        list_directory();
     return (EXIT_SUCCESS);
 }
